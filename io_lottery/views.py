a2bc6f7d37e5a447b642290998eb3d8466d4809e
JakubKapica <u36_jakkap_waw@technischools.com>
2023-04-25 09:16:31 +0200

    Update views.py

--- io_lottery/views.py
+++ io_lottery/views.py
@@ -2,5 +2,22 @@ from flask.views import MethodView
 
 
 class UserView(MethodView):
+
+    def __init__(self, controller):
+        raise NotImplementedError
+
+    def get(self, id):
+        raise NotImplementedError
+
+    # modyfikacja częściowa
+    def patch(self, id):
+        raise NotImplementedError
+
+    def delete(self, id):
+        raise NotImplementedError
+
+    def put(self) -> None:
+        raise NotImplementedError
+
     def post(self) -> None:
         raise NotImplementedError
